1f440519b6066ca354f7f1377e6b329ada691cf0
KostyukS <katarsis1234@yandex.ru>
2022-06-05 21:14:03 +0300

    class VKUser

--- class_VKUser.py
+++ class_VKUser.py
@@ -49,7 +49,7 @@ class VKUser:
         res = res.json()['response'][0].get('deactivated')
         return res
 
-    def Resp(self):
+    def Response_check(self):
         url_check = self.url + 'users.get'
         params = {
             'user_ids': self.id_VK,
